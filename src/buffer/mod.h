b40257ff1fd4cd1d565a9c3cf4cee629b87cd5f0
Cem GEÇGEL <gecgelcem@outlook.com>
2022-06-28 17:01:08 +0300

    add: Global Buffer Module

--- src/buffer/mod.h
+++ src/buffer/mod.h
@@ -4,4 +4,26 @@
 #ifndef CTHRICE_BUFFER_H
 #define CTHRICE_BUFFER_H 1
 
+#include "string/mod.h"
+
+#include <stdint.h>
+
+/** Create the buffer. */
+void cthrice_buffer_create();
+/** Destroy the buffer. */
+void cthrice_buffer_destroy();
+
+/** Get the current end of the buffer. This should be used for keeping track of
+ * where you started to write to the buffer. */
+const char* cthrice_buffer_pos();
+
+/** Append a character to the buffer. */
+void cthrice_buffer_append_chr(char chr);
+/** Append an unsigned integer to the buffer. */
+void cthrice_buffer_append_unt(uint64_t unt);
+/** Append a string to the buffer. */
+void cthrice_buffer_append_string(Cthrice_String str);
+/** Append a file to the buffer. */
+void cthrice_buffer_append_file(const char* pth);
+
 #endif // CTHRICE_BUFFER_H
---
+++ src/error.h
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: (C) 2022 Cem Geçgel <gecgelcem@outlook.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef CTHRICE_ERROR_H
+#define CTHRICE_ERROR_H 1
+
+#include <stdint.h>
+#include <stdio.h>
+
+void cthrice_error(const char* file, uint64_t line, const char* msg)
+{
+    fprintf(stderr, "%s:%llu: error: %s\n", file, line, msg);
+    exit(1);
+}
+
+#define cthrice_check(cnd, msg)                     \
+    do {                                            \
+        if (cnd) {                                  \
+            cthrice_error(__FILE__, __LINE__, msg); \
+        }                                           \
+    } while (0)
+
+#endif // CTHRICE_ERROR_H
