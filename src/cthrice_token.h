60038e39090c15aee892d3cb37432ab638a7926d
Cem GEÇGEL <gecgelcem@outlook.com>
2022-06-29 16:32:53 +0300

    doc: Licensing with Reuse

--- src/cthrice_token.h
+++ src/cthrice_token.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: (C) 2022 Cem Geçgel <gecgelcem@outlook.com>
+// SPDX-FileCopyrightText: 2022 Cem Geçgel <gecgelcem@outlook.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #ifndef CTHRICE_TOKEN_H
