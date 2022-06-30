38701c55a63ca7ac4618c0194f679fa0cb7e971d
Cem GEÇGEL <gecgelcem@outlook.com>
2022-06-30 11:07:46 +0300

    add: Internal Structure of Pattern

--- src/pattern/internal.hh
+++ src/pattern/internal.hh
@@ -7,11 +7,45 @@
 #include "range.hh"
 #include "string/api.hh"
 
+#include <cstddef>
+#include <cstdint>
+
 namespace cthrice
 {
+    /** Implementation of the pattern. This is a graph where edges and verticies
+     * are stored in an array. */
     struct Pattern {
-        String name;
-        // TODO: Implement compiled pattern.
+        /** Type of the pattern. */
+        enum Type {
+            /** Edge in the tree that checks a character. Invalid character
+             * means move to the vertex pointed by this edge without checking
+             * and consuming a character. */
+            EDGE,
+            /** Vertex in the tree that records how many edges come after this
+             * vertex. Zero edges means the matching finished with success. */
+            VERTEX,
+            /** Marker is the start of a tree, stores the name and the
+             * visibility. The pattern can be matched only if it is visible. */
+            MARKER
+        };
+
+        /** Data of the pattern. */
+        union Data {
+            struct Edge {
+                static constexpr int64_t INVALID = -1;
+                int64_t                  literal;
+            };
+            struct Vertex {
+                size_t edges;
+            };
+            struct Marker {
+                String name;
+                bool   visible;
+            };
+        };
+
+        Type type;
+        Data data;
     };
 } // namespace cthrice
 
