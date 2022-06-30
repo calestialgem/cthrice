e0d315c8caf581d59aab98854a4477615773f69d
Cem GEÇGEL <gecgelcem@outlook.com>
2022-06-30 09:40:23 +0300

    add: Pattern Module

---
+++ src/pattern/internal.hh
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2022 Cem Geçgel <gecgelcem@outlook.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef INTERNAL_HH
+#define INTERNAL_HH 1
+
+namespace cthrice
+{
+
+} // namespace cthrice
+
+#endif // INTERNAL_HH
---
+++ src/pattern/mod.hh
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2022 Cem Geçgel <gecgelcem@outlook.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef CTHRICE_PATTERN_HH
+#define CTHRICE_PATTERN_HH 1
+
+namespace cthrice
+{
+
+} // namespace cthrice
+
+#endif // CTHRICE_PATTERN_HH
