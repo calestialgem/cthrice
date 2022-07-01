f24cc13afd1e2cd0acbed555b722a26efa9c6487
Cem GEÇGEL <gecgelcem@outlook.com>
2022-07-01 14:31:49 +0300

    add: Parsing Character Literal Patterns

--- src/pattern/parser.cc
+++ src/pattern/parser.cc
@@ -3,7 +3,9 @@
 
 #include "api.hh"
 #include "buffer.hh"
+#include "error.hh"
 #include "internal.hh"
+#include "range.hh"
 #include "string/api.hh"
 
 namespace cthrice
@@ -28,8 +30,52 @@ namespace cthrice
             .data = {.marker = {.name = name, .visible = visible}}};
     }
 
+    struct ParseContext {
+        Buffer<Pattern> bfr;
+        String          ptrn;
+    };
+
+    [[nodiscard]] static ParseContext literal(ParseContext ctx)
+    {
+        ctx.ptrn.bgn++;
+        cthrice_check(
+            size(ctx.ptrn) == 0,
+            "No character literal in pattern after opening "
+            "quotes!");
+
+        // Vertex that marks a character literal.
+        ctx.bfr = put(ctx.bfr, create(1));
+        ctx.bfr = put(ctx.bfr, create(*ctx.ptrn.bgn, size(ctx.bfr) + 1));
+        ctx.ptrn.bgn++;
+
+        cthrice_check(
+            size(ctx.ptrn) < 2,
+            "No closing quotes in pattern after character "
+            "literal!");
+        ctx.ptrn.bgn++;
+
+        return ctx;
+    }
+
     Buffer<Pattern> parse(Buffer<Pattern> bfr, String name, String ptrn)
     {
+        // Vertex that marks the start of a pattern.
+        bfr = put(bfr, create(name, contains(name, '@')));
+
+        ParseContext ctx = {.bfr = bfr, .ptrn = ptrn};
+
+        while (size(ptrn) != 0) {
+            switch (*ptrn.bgn) {
+                case '\'':
+                    ctx = literal(ctx);
+                    break;
+                default:
+                    cthrice_check(true, "Unexpected character in pattern!");
+            }
+        }
+
+        // Vertex that marks the match.
+        bfr = put(bfr, create(0));
         return bfr;
     }
 } // namespace cthrice
