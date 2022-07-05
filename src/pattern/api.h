0ea91ad86ecf067b3e1fd5e26e8af56e358c4ede
Cem GEÇGEL <gecgelcem@outlook.com>
2022-07-05 19:40:31 +0300

    refactor: Pattern API

--- src/pattern/api.h
+++ src/pattern/api.h
@@ -7,27 +7,61 @@
 #include "buffer/api.h"
 #include "string/api.h"
 
-/* Compiled pattern. */
-struct ptrn;
+/* Compiled pattern byte code. */
+struct ptrncode;
+
+/* Pattern information. */
+struct ptrn {
+    /* Identifier. */
+    struct str name;
+    /* Start of the code. */
+    struct ptrncode* code;
+};
 
 /* Dynamic array of patterns. */
 struct ptrns {
     /* Buffer that holds the pattern names. */
     struct bfr bfr;
-    /* Pointer to the first allocated pattern. */
-    struct ptrn* bgn;
-    /* Pointer to the pattern after the last valid one. */
-    struct ptrn* end;
-    /* Pointer to the pattern after the last allocated one. */
-    struct ptrn* lst;
+
+    /* Fields related to pattern name hashes. */
+    struct {
+        /* Pointer to the first allocated hash. */
+        struct ptrn** bgn;
+        /* Pointer to the hash after the last valid and allocated one. */
+        struct ptrn** end;
+    } hash;
+
+    /* Fields related to pattern information. */
+    struct {
+        /* Pointer to the first allocated pattern. */
+        struct ptrn* bgn;
+        /* Pointer to the pattern after the last valid one. */
+        struct ptrn* end;
+        /* Pointer to the pattern after the last allocated one. */
+        struct ptrn* lst;
+    } values;
+
+    /* Fields related to compiled pattern code. */
+    struct {
+        /* Pointer to the first allocated code. */
+        struct ptrncode* bgn;
+        /* Pointer to the code after the last valid one. */
+        struct ptrncode* end;
+        /* Pointer to the code after the last allocated one. */
+        struct ptrncode* lst;
+    } code;
 };
 
 /* Parse the pattern by searching for references in the patterns. */
 struct ptrns ptrn_parse(struct ptrns, struct str);
 
-/* Match the string to the patterns. Returns the name of the maching pattern.
- * Returns empty string if nothing matches. */
+/* Match the string to the patterns. Returns the name of a maching pattern.
+ * Returns empty string if nothing matches. If the string matches to multiple
+ * patterns the one returned is undeterministic. */
 struct str ptrn_match(struct ptrns, struct str);
+
+/* Check the string to the pattern with the give name. */
+bool ptrn_check(struct ptrns, struct str, struct str);
 
 /* Deallocate the patterns. */
 struct ptrns ptrn_destory(struct ptrns);
