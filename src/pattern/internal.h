426cd17343347b45bcde11bada65731b8ca74750
Cem GEÇGEL <gecgelcem@outlook.com>
2022-07-05 20:17:44 +0300

    add: Pattern Debug Printing

--- src/pattern/internal.h
+++ src/pattern/internal.h
@@ -4,7 +4,11 @@
 #ifndef INTERNAL_H
 #define INTERNAL_H 1
 
+#include "pattern/api.h"
+#include "string/api.h"
 #include "types/api.h"
+
+#include <stdio.h>
 
 /** Implementation of the pattern code. These are the transitions in a
  * nondeterministic finite automaton with empty moves. */
@@ -39,5 +43,51 @@ struct ptrncode {
         ptr off;
     };
 };
+
+/* Print the pattern information. */
+static void print_patterns(struct ptrns ptrns)
+{
+    printf("\n  PATTERNS\n============\n\n");
+    for (const struct ptrn* i = ptrns.values.bgn; i < ptrns.values.end; i++) {
+        printf(
+            "Pattern {%.*s} [%05lld]\n",
+            (int)str_size(i->name),
+            i->name.bgn,
+            i->code);
+    }
+}
+
+/* Print the code with its type and data. */
+static void print_code(struct ptrncode code)
+{
+    switch (code.type) {
+        case EMPTY:
+            printf("EMPTY");
+            break;
+        case LITERAL:
+            printf("LITERAL {%c}", code.ltrl);
+            break;
+        case RANGE:
+            printf("RANGE {%c~%c}", code.bgn, code.end);
+            break;
+        case PATTERN:
+            printf("PATTERN {%05lld}", code.off);
+            break;
+        case MATCH:
+            printf("MATCH");
+            break;
+    }
+    printf(" %+lld\n", code.move);
+}
+
+/* Print the codes in the pattern. */
+static void print_codes(struct ptrns ptrns)
+{
+    printf("\n  PATTERN CODE\n================\n\n");
+    for (const struct ptrncode* i = ptrns.code.bgn; i < ptrns.code.end; i++) {
+        printf("[%05lld] ", i - ptrns.code.bgn);
+        print_code(*i);
+    }
+}
 
 #endif // INTERNAL_H
