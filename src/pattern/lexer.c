c787260c693e51869c1915e9e3ea270ea87eef89
Cem GEÇGEL <gecgelcem@outlook.com>
2022-07-07 15:18:07 +0300

    add: Pattern Lex Destroy

--- src/pattern/lexer.c
+++ src/pattern/lexer.c
@@ -3,7 +3,15 @@
 
 #include "api.h"
 #include "internal.h"
+#include "types/api.h"
+
+#include <stdlib.h>
 
 struct ptrnlex ptrn_lex(struct str) {}
 
-struct ptrnlex ptrn_lex_destroy(struct ptrnlex) {}
+struct ptrnlex ptrn_lex_destroy(struct ptrnlex lex)
+{
+    free(lex.bgn);
+    lex.bgn = lex.end = lex.lst = null;
+    return lex;
+}
