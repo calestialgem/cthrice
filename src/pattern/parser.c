e7b79bcb30df0401ef1fd2141fb54abe4b0e89b0
Cem GEÇGEL <gecgelcem@outlook.com>
2022-07-07 18:25:31 +0300

    add: Pattern Name Parsing

--- src/pattern/parser.c
+++ src/pattern/parser.c
@@ -2,8 +2,53 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "api.h"
+#include "buffer/api.h"
+#include "error/api.h"
 #include "internal.h"
 #include "string/api.h"
+
+#include <stdio.h>
+
+/* Current state of the parse. */
+struct parse {
+    /* Pattern context. */
+    struct ptrnctx ctx;
+    /* Name of the parsed pattern. */
+    struct str name;
+    /* Token array. */
+    struct ptrnlex lex;
+    /* The token that is currently on the carret. */
+    struct ptrntkn* crt;
+};
+
+/* Find the remaining amount of tokens including the carret. */
+static ix remaining(struct parse p)
+{
+    return p.lex.end - p.crt;
+}
+
+/* Parse the name of the pattern. */
+static struct parse parse_name(struct parse p)
+{
+    CHECK(
+        remaining(p) >= 3,
+        "There should be atleast a name, an equal sign and a unit!");
+
+    CHECK(
+        p.crt->typ == TOKEN_IDENTIFIER,
+        "Pattern definition must start with the identifier!");
+    p.name = p.crt->val;
+    p.crt++;
+
+    CHECK(
+        p.crt->typ == TOKEN_EQUAL,
+        "Pattern definition must continue with an equal sign!");
+    p.crt++;
+
+    // TODO: Copy the name to the buffer.
+
+    return p;
+}
 
 struct ptrnctx ptrn_parse(struct ptrnctx ctx, struct str ptrn)
 {
@@ -12,10 +57,11 @@ struct ptrnctx ptrn_parse(struct ptrnctx ctx, struct str ptrn)
     // DEBUG: Print lex.
     ptrn_print_lex(lex);
 
-    // TODO: Parse the lex.
+    struct parse p = {.ctx = ctx, .lex = lex, .crt = lex.bgn};
+    p              = parse_name(p);
 
-    lex = ptrn_lex_destroy(lex);
-    return ctx;
+    p.lex = ptrn_lex_destroy(p.lex);
+    return p.ctx;
 }
 
 struct ptrnctx ptrn_destory(struct ptrnctx ctx)
