c55ab8086083a326127ce1916ec130f8efdd4427
Cem GEÇGEL <gecgelcem@outlook.com>
2022-07-12 10:37:56 +0300

    clean: Project

--- src/main.cc
+++ src/main.cc
@@ -1,34 +1,15 @@
 // SPDX-FileCopyrightText: 2022 Cem Geçgel <gecgelcem@outlook.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include "prelude/list.cc"
 #include "prelude/scalar.cc"
 
-#include <array>
 #include <cstdio>
 
 using namespace cthrice;
 
-/* Print the character slice. */
-constexpr void println(Slice<c8 const> const& str) noexcept
-{
-    std::printf("%.*s\n", (int)str.size(), str.get(0));
-}
-
 /* Entry to the compiler. */
 i32 main(i32 const, c8 const* const* const) noexcept
 {
-    List<Slice<c8 const>> strs;
-
-    strs.add(Slice{"Hello, hello, hello!"}.split(',').lead);
-    strs.add(Slice{"Hello, hello, hello!"}.split('h').trail);
-    strs.add(Slice{"Hello, hello, hello!"}.split('!').trail);
-    strs.add(Slice{"Hello, hello, hello!"}.split('a').lead);
-    strs.add(Slice{"Hello, hello, hello!"}.split('a').trail);
-
-    for (const Slice<c8 const>* i{strs.bgn}; i < strs.end; i++) {
-        println(*i);
-    }
-
+    std::printf("Hello, hello, hello!\n");
     return 0;
 }
--- src/prelude/scalar.cc
+++ src/prelude/scalar.cc
@@ -8,37 +8,37 @@
 
 namespace cthrice
 {
-    /* Index type. Pointer arithmetic should be done with this type. It is
-     * better than using an unsigned type like size_t. If the pointer difference
-     * cannot fit into ptrdiff_t, it is undefined behaviour anyways. */
-    using ix = std::ptrdiff_t;
+/* Index type. Pointer arithmetic should be done with this type. It is
+ * better than using an unsigned type like size_t. If the pointer difference
+ * cannot fit into ptrdiff_t, it is undefined behaviour anyways. */
+using ix = std::ptrdiff_t;
 
-    /* Signed, 8-bit integer. */
-    using i8 = std::int8_t;
-    /* Signed, 16-bit integer. */
-    using i16 = std::int16_t;
-    /* Signed, 32-bit integer. */
-    using i32 = std::int32_t;
-    /* Signed, 64-bit integer. */
-    using i64 = std::int64_t;
+/* Signed, 8-bit integer. */
+using i8 = std::int8_t;
+/* Signed, 16-bit integer. */
+using i16 = std::int16_t;
+/* Signed, 32-bit integer. */
+using i32 = std::int32_t;
+/* Signed, 64-bit integer. */
+using i64 = std::int64_t;
 
-    /* Unsigned, 8-bit integer. */
-    using u8 = std::uint8_t;
-    /* Unsigned, 16-bit integer. */
-    using u16 = std::uint16_t;
-    /* Unsigned, 32-bit integer. */
-    using u32 = std::uint32_t;
-    /* Unsigned, 64-bit integer. */
-    using u64 = std::uint64_t;
+/* Unsigned, 8-bit integer. */
+using u8 = std::uint8_t;
+/* Unsigned, 16-bit integer. */
+using u16 = std::uint16_t;
+/* Unsigned, 32-bit integer. */
+using u32 = std::uint32_t;
+/* Unsigned, 64-bit integer. */
+using u64 = std::uint64_t;
 
-    /* Floating point, 32-bit real. */
-    using f32 = float;
-    /* Floating point, 64-bit real. */
-    using f64 = double;
+/* Floating point, 32-bit real. */
+using f32 = float;
+/* Floating point, 64-bit real. */
+using f64 = double;
 
-    /* Smallest memory unit, which is a signed, 8-bit integer. */
-    using c8 = char;
+/* Smallest memory unit, which is a signed, 8-bit integer. */
+using c8 = char;
 
-    /* Boolean algebra type, which is an 8-bit integer. */
-    using b8 = bool;
+/* Boolean algebra type, which is an 8-bit integer. */
+using b8 = bool;
 } // namespace cthrice
