8d2153988675c2d483a81821fe4eddb8f3044fa4
Cem Geçgel <gecgelcem@outlook.com>
2022-07-16 17:02:14 +0300

    add: Reading Arguments

--- src/main.c
+++ src/main.c
@@ -3,9 +3,30 @@
 
 #include <stdio.h>
 
+/* Compile the source file at the path. */
+void compile(char const* const path)
+{
+    printf("Compiling %s...\n", path);
+}
+
 /* Entry to the compiler. */
 int main(int const argumentCount, char const* const* const arguments)
 {
-    printf("Hello, hello, hello!\n");
+    printf("Thrice C Transpiler\n");
+    printf("Running with arguments:\n");
+    for (int i = 0; i < argumentCount; i++) {
+        printf("[%d] {%s}\n", i, arguments[i]);
+    }
+    printf("\n");
+
+    if (argumentCount < 2) {
+        fprintf(stderr, "Provide a thrice file!");
+        return -1;
+    }
+
+    for (int i = 1; i < argumentCount; i++) {
+        compile(arguments[i]);
+    }
+
     return 0;
 }
