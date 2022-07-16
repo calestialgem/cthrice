67600a97f88ac3bd08da53c4c3d610f8fc48c020
Cem Geçgel <gecgelcem@outlook.com>
2022-07-16 09:59:54 +0300

    add: Prelude List Range Capability

--- src/prelude/list.cc
+++ src/prelude/list.cc
@@ -15,4 +15,18 @@ struct List {
     /* Border after the last allocated element. */
     Element* allocated;
 };
+
+/* Border before the first element of the list. */
+template<typename Element>
+[[nodiscard]] Element* first(List<Element> const& list) noexcept
+{
+    return list.first;
+}
+
+/* Border after the last element of the list. */
+template<typename Element>
+[[nodiscard]] Element* last(List<Element> const& list) noexcept
+{
+    return list.last;
+}
 } // namespace cthrice
