fcd8451cacbc56b3a906606e25a69707024bcd67
Cem Geçgel <gecgelcem@outlook.com>
2022-07-16 09:40:18 +0300

    add: Prelude Range

---
+++ src/prelude/range.cc
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2022 Cem Geçgel <gecgelcem@outlook.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <concepts>
+
+namespace cthrice
+{
+/* Pointers to contiguous range of elements. */
+template<typename Type, typename Element>
+concept Range = requires(Type range)
+{
+    {
+        range.first
+        } -> std::convertible_to<Element*>;
+    {
+        range.last
+        } -> std::convertible_to<Element*>;
+};
+} // namespace cthrice
