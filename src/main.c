8db2538fb7234f7bcdffbaf7f158e3941dc6a9b3
Cem Geçgel <gecgelcem@outlook.com>
2022-07-16 16:57:12 +0300

    change: Language Back to C

---
+++ src/main.c
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2022 Cem Geçgel <gecgelcem@outlook.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <stdio.h>
+
+/* Entry to the compiler. */
+int main(int const argumentCount, char const* const* const arguments)
+{
+    printf("Hello, hello, hello!\n");
+    return 0;
+}
