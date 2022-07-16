61a1a1993535eb657a340d4f8b92d356bfd8a93b
Cem Geçgel <gecgelcem@outlook.com>
2022-07-16 11:38:48 +0300

    add: Prelude List Reserve

--- src/prelude/list.cc
+++ src/prelude/list.cc
@@ -3,6 +3,13 @@
 
 #pragma once
 
+#include "prelude/expect.cc"
+#include "prelude/range.cc"
+#include "prelude/scalar.cc"
+
+#include <algorithm>
+#include <cstdlib>
+
 namespace cthrice
 {
 /* Dynamic array. */
@@ -28,5 +35,41 @@ template<typename Element>
 [[nodiscard]] constexpr Element* last(List<Element> const& list) noexcept
 {
     return list.last;
+}
+
+/* Amount of allocated elements. */
+template<typename Element>
+[[nodiscard]] constexpr ix capacity(List<Element> const& list) noexcept
+{
+    return list.allocated - list.first;
+}
+
+/* Amount of allocated but unused elements. */
+template<typename Element>
+[[nodiscard]] constexpr ix space(List<Element> const& list) noexcept
+{
+    return list.allocated - list.last;
+}
+
+/* Make sure the space is at least as big as the amount. Grows if necessary by
+ * at least the half of the current capacity. */
+template<typename Element>
+constexpr void reserve(List<Element>& list, ix amount) noexcept
+{
+    expect(amount >= 0, "Reserving negative amount!");
+    ix growth{amount - space(list)};
+    if (growth <= 0) {
+        return;
+    }
+
+    ix       capacity{cthrice::capacity(list)};
+    ix       newCapacity{capacity + std::max(growth, capacity >> 1)};
+    Element* memory{
+        (Element*)std::realloc(list.first, sizeof(Element) - newCapacity)};
+    expect(memory != nullptr, "Could not allocate!");
+
+    list.last      = memory + size(list);
+    list.first     = memory;
+    list.allocated = memory + newCapacity;
 }
 } // namespace cthrice
