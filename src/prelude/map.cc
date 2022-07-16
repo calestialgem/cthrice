1cedc3dda18b11a0a70f36307d96734902ee5be8
Cem Geçgel <gecgelcem@outlook.com>
2022-07-16 09:56:07 +0300

    add: Prelude Map

---
+++ src/prelude/map.cc
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2022 Cem Geçgel <gecgelcem@outlook.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include "prelude/hashable.cc"
+#include "prelude/list.cc"
+#include "prelude/scalar.cc"
+
+namespace cthrice
+{
+/* Mapping from hashable keys to values. */
+template<Hashable Key, typename Value>
+struct Map {
+    /* Pair of a key and its value. */
+    struct Pair {
+        /* Hashable key. */
+        Key key;
+        /* Corresponding value. */
+        Value value;
+    };
+    /* Pair indicies that correspond to hashes. */
+    List<ix> indicies;
+    /* Data. */
+    List<Pair> pairs;
+};
+} // namespace cthrice
