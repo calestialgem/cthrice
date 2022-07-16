00bb9a759dbfb13a9fcb11ad3b7c8eea92e0e4e4
Cem Geçgel <gecgelcem@outlook.com>
2022-07-16 18:44:44 +0300

    add: Prelude String Equal and Hash

--- src/prelude/string.c
+++ src/prelude/string.c
@@ -5,6 +5,8 @@
 
 #include "prelude/scalar.c"
 
+#include <stdbool.h>
+
 /* Immutable pointers to contiguous characters. */
 typedef struct {
     /* Border before the first character. */
@@ -17,4 +19,35 @@ typedef struct {
 CTIndex ct_string_size(CTString const* string)
 {
     return string->last - string->first;
+}
+
+/* Whether the strings are the same. */
+bool ct_string_equal(CTString const* lhs, CTString const* rhs)
+{
+    CTIndex size = ct_string_size(lhs);
+    if (size != ct_string_size(rhs)) {
+        return false;
+    }
+    for (CTIndex i = 0; i < size; i++) {
+        if (lhs->first[i] != rhs->first[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/* Prime number used in string hashing. Upper and lower case letters in the
+ * English alphabet gives 26 character each, and there is underscore which sums
+ * to 53 characters. */
+#define CT_STRING_PRIME 53
+
+/* Hash of the string. */
+unsigned long long ct_string_hash(CTString const* string)
+{
+    unsigned long long hash = 0;
+    for (char const* i = string->first; i < string->last; i++) {
+        hash *= CT_STRING_PRIME;
+        hash += *i;
+    }
+    return hash;
 }
