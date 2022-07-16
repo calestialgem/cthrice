c329c4e94f9cd0e03aecba73fe0d339178fd6344
Cem Geçgel <gecgelcem@outlook.com>
2022-07-16 21:58:40 +0300

    add: Lexing Tokens File

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
 // SPDX-FileCopyrightText: 2022 Cem Geçgel <gecgelcem@outlook.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include "patlak/lexer.c"
+#include "patlak/printer.c"
+#include "patlak/token.c"
 #include "prelude/buffer.c"
 #include "prelude/expect.c"
 #include "prelude/file.c"
+#include "prelude/split.c"
 #include "prelude/string.c"
 
 #include <stdio.h>
@@ -14,7 +18,25 @@ void ct_compile(char const* path)
     printf("Compiling %s...\n", path);
     CTBuffer buffer = {0};
     CTString file   = ct_file_load(&buffer, path);
-    printf("%.*s\n", (int)ct_string_size(&file), file.first);
+
+    CTPatlakTokens tokens = {0};
+
+    while (ct_string_finite(&file)) {
+        CTSplit  split = ct_split_first(&file, '\n');
+        CTString line  = split.before;
+        file.first     = split.after.first + 1;
+
+        if (ct_string_size(&line) >= 2 && ct_string_at(&line, 0) == '/' &&
+            ct_string_at(&line, 1) == '/') {
+            continue;
+        }
+
+        ct_patlak_tokens_clear(&tokens);
+        ct_patlak_lexer(&tokens, line);
+        ct_patlak_printer_tokens(&tokens);
+    }
+
+    ct_patlak_tokens_free(&tokens);
     ct_buffer_free(&buffer);
 }
 
