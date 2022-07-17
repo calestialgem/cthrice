250d1a9c37b95015a1d91b4af9e9b9ea7394c8dc
Cem Geçgel <gecgelcem@outlook.com>
2022-07-18 02:42:42 +0300

    remove: File Test

--- src/main.c
+++ src/main.c
@@ -15,33 +15,33 @@
 
 #include <stdio.h>
 
-/* Compile the source file at the path. */
-void ct_compile(char const* path)
-{
-    printf("Compiling %s...\n", path);
-    CTBuffer buffer = {0};
-    CTString file   = ct_file_load(&buffer, path);
+// /* Compile the source file at the path. */
+// void ct_compile(char const* path)
+// {
+//     printf("Compiling %s...\n", path);
+//     CTBuffer buffer = {0};
+//     CTString file   = ct_file_load(&buffer, path);
 
-    CTPatlakTokens tokens = {0};
+// CTPatlakTokens tokens = {0};
 
-    while (ct_string_finite(&file)) {
-        CTSplit  split = ct_split_first(&file, '\n');
-        CTString line  = split.before;
-        file.first     = split.after.first + 1;
+// while (ct_string_finite(&file)) {
+//     CTSplit  split = ct_split_first(&file, '\n');
+//     CTString line  = split.before;
+//     file.first     = split.after.first + 1;
 
-        if (ct_string_size(&line) >= 2 && ct_string_at(&line, 0) == '/' &&
-            ct_string_at(&line, 1) == '/') {
-            continue;
-        }
+// if (ct_string_size(&line) >= 2 && ct_string_at(&line, 0) == '/' &&
+//     ct_string_at(&line, 1) == '/') {
+//     continue;
+// }
 
-        ct_patlak_tokens_clear(&tokens);
-        ct_patlak_lexer(&tokens, line);
-        ct_patlak_printer_tokens(&tokens);
-    }
+// ct_patlak_tokens_clear(&tokens);
+// ct_patlak_lexer(&tokens, line);
+// ct_patlak_printer_tokens(&tokens);
+//     }
 
-    ct_patlak_tokens_free(&tokens);
-    ct_buffer_free(&buffer);
-}
+// ct_patlak_tokens_free(&tokens);
+// ct_buffer_free(&buffer);
+// }
 
 /* Entry to the compiler. */
 int main(int argument_count, char const* const* arguments)
@@ -53,10 +53,10 @@ int main(int argument_count, char const* const* arguments)
     }
     printf("\n");
 
-    ct_expect(argument_count >= 2, "Provide a thrice file!");
-    for (int i = 1; i < argument_count; i++) {
-        ct_compile(arguments[i]);
-    }
+    // ct_expect(argument_count >= 2, "Provide a thrice file!");
+    // for (int i = 1; i < argument_count; i++) {
+    //     ct_compile(arguments[i]);
+    // }
 
     CTString pattern = ct_string_terminated(
         "hello = [5]{ 'Hello!'} 'some other text' {'after' 'that' 'text' } ");
@@ -66,7 +66,6 @@ int main(int argument_count, char const* const* arguments)
     ct_patlak_lexer(&tokens, pattern);
     ct_patlak_parser(&tree, &tokens);
     ct_patlak_printer_tree(&tree);
-    ct_patlak_printer_objects(&tree);
 
     ct_patlak_tree_free(&tree);
     ct_patlak_tokens_free(&tokens);
