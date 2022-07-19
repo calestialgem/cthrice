8b65bd7bd28def987376be952437628fd995775f
Cem GEÇGEL <gecgelcem@outlook.com>
2022-07-19 13:53:22 +0300

    add: Printing Arguments

--- src/main.cc
+++ src/main.cc
@@ -2,9 +2,33 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include <iostream>
+#include <span>
+#include <string_view>
+#include <vector>
 
-auto main() -> int
+/* Arguments given to the program. */
+using Arguments = std::span<char const* const>;
+
+/* Print the arguments. */
+auto print_arguments(Arguments const& arguments) noexcept
 {
-    std::cout << "Hello, hello, hello!" << std::endl;
+    std::cout << "Thrice C Transpiler\n"
+              << "Running with arguments:\n";
+    for (std::size_t i{0}; i < arguments.size(); i++) {
+        std::cout << '[' << i << "] {" << arguments[i] << "}\n";
+    }
+    std::cout << std::endl;
+}
+
+/* Launch the compiler. */
+auto main(
+    int const                argumentCount,
+    char const* const* const argumentValues) noexcept -> int
+{
+    std::span<char const* const> const arguments{
+        argumentValues,
+        static_cast<std::size_t>(argumentCount)};
+    print_arguments(arguments);
+
     return 0;
 }
