877cff769361fa36f2110fcb4ffc31039a287dff
Cem Geçgel <gecgelcem@outlook.com>
2022-07-21 18:49:05 +0300

    add: Patlak Compiler

---
+++ src/patlak/compiler.c
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2022 Cem Geçgel <gecgelcem@outlook.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include "patlak/context.c"
+#include "patlak/object.c"
+
+/* Compile the syntax tree to the context. */
+void ct_patlak_compiler(CTPatlakContext* context, CTPatlakTree const* tree) {}
