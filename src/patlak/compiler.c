ddd5a04d7b3eca6330556f471591fd2d5a0b1051
Cem Geçgel <gecgelcem@outlook.com>
2022-07-21 18:54:57 +0300

    add: Patlak Compiler Empty Implementation

--- src/patlak/compiler.c
+++ src/patlak/compiler.c
@@ -5,6 +5,24 @@
 
 #include "patlak/context.c"
 #include "patlak/object.c"
+#include "prelude/scalar.c"
 
 /* Compile the syntax tree to the context. */
-void ct_patlak_compiler(CTPatlakContext* context, CTPatlakTree const* tree) {}
+void ct_patlak_compiler(CTPatlakContext* context, CTPatlakTree const* tree)
+{
+    // Store the start index to put into the hashmap.
+    CTIndex start = ct_patlak_codes_size(&context->codes);
+
+    // TODO: Compile the tree.
+
+    // Add the code that means a match to the end.
+    ct_patlak_codes_add(
+        &context->codes,
+        (CTPatlakCode){.movement = 0, .type = CT_PATLAK_CODE_TERMINAL});
+
+    // Put the pattern to the hashmap.
+    ct_patlak_patterns_add(
+        &context->patterns,
+        &tree->first[1].object.value,
+        start);
+}
